56af3dc7b0c7dad77b90515e8921beb43451b432
xmonkey02 <65108714+xmonkey02@users.noreply.github.com>
2023-11-12 18:28:39 +0800

    Change SQL command and add commnet funtion.
    Update: 1.All table sql command is checked.
    	2.issuedetail and uersdetail is modified.
    	3.Add comment function in issuedetail page.
    
    Reason: Need correct SQL command.
    	We need comment function.
    
    Signed-off-by: xmonkey02 <65108714+xmonkey02@users.noreply.github.com>

--- TASK.sql
+++ TASK.sql
@@ -1,41 +1,59 @@
-CREATE TABLE TRACKUSER
+CREATE TABLE Group5.TRACKUSER
 (
-    userId CHAR(8) NOT NULL,
-    userName VARCHAR(255) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    "userId" CHAR(8) PRIMARY KEY,
+    userName VARCHAR2(255) NOT NULL,
+    password VARCHAR2(50) NOT NULL,
     supervisorId CHAR(8),
     adderId CHAR(8) NOT NULL,
-    PRIMARY KEY (userId),
-    FOREIGN KEY (supervisorId) REFERENCES TRACKUSER(userId),
-    FOREIGN KEY (adderId) REFERENCES TRACKUSER(userId)
+    FOREIGN KEY (supervisorId) REFERENCES Group5.TRACKUSER("userId"),
+    FOREIGN KEY (adderId) REFERENCES Group5.TRACKUSER("userId")
 );
 
-CREATE TABLE TASK
+CREATE TABLE Group5.Task
 (
-    taskId CHAR(8) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    description NVARCHAR(MAX),
+    "taskId" CHAR(8) PRIMARY KEY,
+    status VARCHAR2(20) NOT NULL,
+    description CLOB,
     taskOwner CHAR(8),
-    title CHAR(20),
-    dueDate DATETIME,
+    title VARCHAR2(20),
+    dueDate TIMESTAMP,
     assigner CHAR(8),
     creator CHAR(8) NOT NULL,
-    assignTime DATETIME,
-    PRIMARY KEY (taskId),
-    FOREIGN KEY (taskOwner) REFERENCES TRACKUSER(userId),
-    FOREIGN KEY (assigner) REFERENCES TRACKUSER(userId),
-    FOREIGN KEY (creator) REFERENCES TRACKUSER(userId)
+    assignTime TIMESTAMP,
+    FOREIGN KEY (taskOwner) REFERENCES Group5.TRACKUSER("userId"),
+    FOREIGN KEY (assigner) REFERENCES Group5.TRACKUSER("userId"),
+    FOREIGN KEY (creator) REFERENCES Group5.TRACKUSER("userId")
 );
 
-CREATE TABLE TASKCOMMENT
+CREATE TABLE Group5.TASKCOMMENT
 (
-    commentId CHAR(8) NOT NULL,
+    commentId CHAR(8) PRIMARY KEY,
     commenterId CHAR(8) NOT NULL,
     taskId CHAR(8) NOT NULL,
-    content NVARCHAR(MAX) NOT NULL,
-    commentTime DATETIME NOT NULL,
-    lastUpdateTime DATETIME,
-    PRIMARY KEY (commentId),
-    FOREIGN KEY (commenterId) REFERENCES TRACKUSER(userId),
-    FOREIGN KEY (taskId) REFERENCES TASK(taskId)
+    content CLOB NOT NULL,
+    commentTime TIMESTAMP NOT NULL,
+    lastUpdateTime TIMESTAMP,
+    FOREIGN KEY (commenterId) REFERENCES Group5.TRACKUSER("userId"),
+    FOREIGN KEY (taskId) REFERENCES Group5.Task("taskId")
+);
+
+
+CREATE TABLE group5.Feature
+(
+    featureId CHAR(8) PRIMARY KEY,
+    creatorId CHAR(8) NOT NULL,
+    maintainerId CHAR(8) NOT NULL,
+    title VARCHAR2(20),
+    description CLOB NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES GROUP5.TRACKUSER("userId"),
+    FOREIGN KEY (maintainerId) REFERENCES GROUP5.TRACKUSER("userId")
+);
+
+CREATE TABLE FeatureTaskRelation
+(
+    featureId CHAR(8) NOT NULL,
+    taskId CHAR(8) NOT NULL,
+    taskOrder INT,
+    FOREIGN KEY (featureId) REFERENCES group5.Feature(featureId),
+    FOREIGN KEY (taskId) REFERENCES group5.Task("taskId")
 );
